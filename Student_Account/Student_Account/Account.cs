c19bd9e8f20eb8d7b45f872cdda114bb63dfafb1
fuadmuradov <63427158+fuadmuradov@users.noreply.github.com>
2021-12-08 23:27:08 +0400

    Add files via upload

---
+++ Student_Account/Student_Account/Account.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Account
+{
+    class Account
+    {
+        public string name;
+        public string password;
+        public static int No=0;
+
+        public Account(string name, string password)
+        {
+            this.name = name;
+            this.password = password;
+        }
+
+        public static bool IsUserNameValid(string validname)
+        {
+            foreach (char c in validname)
+            {
+                if (!Char.IsLetterOrDigit(c))
+                    return false;
+            }
+
+            return true;
+        }
+
+        public static bool IsPasswordValid(string validname)
+        {
+            bool symbol = false, uplatter = false, digit = false; 
+
+            if (validname.Length >= 8 && validname.Length <= 25)
+            {
+                foreach (char c in validname)
+                {
+                    if (Char.IsDigit(c)) digit = true;
+                    if (Char.IsSymbol(c)) symbol = true;
+                    if (Char.IsUpper(c)) uplatter = true;
+                       
+                  
+                }
+
+             
+            }
+            else return false;
+
+            if (symbol == true && uplatter == true && digit == true) return true;
+            else
+            return false;
+        }
+
+    }
+}
---
+++ Student_Account/Student_Account/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Account
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please Enter username: ");
+            string name = Console.ReadLine().ToString();
+            Console.WriteLine("Please Enter Password: ");
+            string password = Console.ReadLine().ToString();
+
+            if (Account.IsPasswordValid(password) && Account.IsUserNameValid(name))
+            {
+                Account account = new Account(name, password);
+                Console.WriteLine("Account Created");
+            }
+            else
+            {
+                Console.WriteLine("Username Or Password is not valid");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
