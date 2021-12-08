586902cca0e2dbefc3d5123a682887fd30b01fcd
fuadmuradov <63427158+fuadmuradov@users.noreply.github.com>
2021-12-08 23:25:35 +0400

    Add files via upload

---
+++ Products/Products/Product.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Products
+{
+    class Product
+    {
+        public string code;
+        public string name;
+        public int price;
+        public int discPrice;
+        public static int no = 1000;
+        public Producttype type;
+        public static int totalcount=0;
+        public Product(Producttype type, int price)
+        {
+            string types = Convert.ToString(type);
+            this.type = type;
+            this.price = price;
+            if (type == Producttype.Bakery)
+            {
+                discPrice = (price * 9) / 10;
+            }
+            else
+            {
+                discPrice = price;
+            }
+            code = types[0] + Convert.ToString(no);
+
+            totalcount++;
+            no++;
+        }
+
+    }
+    enum Producttype
+    {
+        Bakery,
+        Drink,
+        Meat,
+        Fish
+    }
+}
---
+++ Products/Products/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Products
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Product product = new Product(Producttype.Bakery, 100);
+            product.name = "Nur";
+
+            Product product1 = new Product(Producttype.Drink, 500);
+            product1.name = "Milla";
+
+            Product product2 = new Product(Producttype.Fish, 900);
+            product2.name = "Pirana";
+
+            Console.WriteLine(product.type + " " +product.name+ " " + product.code + " " + product.discPrice +
+              " " + product.price + " " + Product.totalcount);
+            Console.WriteLine(product1.type + " " + product1.name + " " + product1.code + " " + product1.discPrice +
+              " " + product1.price + " " + Product.totalcount);
+            Console.WriteLine(product2.type + " " + product2.name + " " + product2.code + " " + product2.discPrice +
+             " " + product2.price + " " + Product.totalcount);
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
