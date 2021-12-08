49e620ba6f599ae3c01db70697588380fc1bfd38
fuadmuradov <63427158+fuadmuradov@users.noreply.github.com>
2021-12-08 23:36:31 +0400

    Add files via upload

---
+++ Book_management/Book_management/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book_management
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] num = { 2, 3, 4, 5, 6, 7, 8, 9 };
+            //useParam(num);
+            Student[] students = new Student[2];
+           
+            for (int i = 0; i < students.Length; i++)
+            {
+                Console.WriteLine("Telebe adini daxil edin!");
+                String name = Console.ReadLine();
+
+                Console.WriteLine("Telebenin balini daxil edin!");
+                double point = Convert.ToDouble(Console.ReadLine());
+
+                Student student = new Student(name, point);
+
+
+                students[i] = student;
+
+
+            }
+
+
+          Console.WriteLine($"telebelerin ortalamasi {average(students)}");
+
+            //ArrayList arraylist = new ArrayList();
+            //arraylist.Add("fuad");
+            //arraylist.Add(5);
+            //arraylist.Add(true);
+
+            //foreach (var item in arraylist)
+            //{
+            //    Console.WriteLine($"arraylistin elementleri {item}");
+            //}
+
+            Console.ReadLine();
+        }
+
+        public static double average(Student[] students)
+        {
+            double sum = 0;
+            foreach (var item in students)
+            {
+                sum = sum + item.Point;
+            }
+            return sum / students.Length;
+        }
+
+        //public static int useParam(int[] list)
+        //{
+        //    foreach(var item in list)
+        //    {
+        //        Console.WriteLine(item);
+        //    }
+
+
+        //    return 0;
+        //}
+    }
+}
---
+++ Book_management/Book_management/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book_management
+{
+    class Student
+    {
+        public string Sname;
+        public double Point;
+        public Student(string Sname, double Point)
+        {
+            this.Sname = Sname;
+            this.Point = Point;
+        }
+    }
+}
