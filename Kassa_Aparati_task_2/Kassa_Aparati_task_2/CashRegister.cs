233dc91e209cd767032fc5cbcc251fcffec6429d
fuadmuradov <63427158+fuadmuradov@users.noreply.github.com>
2021-12-08 23:32:12 +0400

    Add files via upload

---
+++ Kassa_Aparati_task_2/Kassa_Aparati_task_2/CashRegister.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kassa_Aparati_task_2
+{
+    class CashRegister
+    {
+        private static double TotalAmount=0;
+        public Currency currency;
+        public Paymenttype paymenttype;
+        private static int TotalSalesCount=0;
+
+
+        public static double totalamount
+        {
+            get { return TotalAmount; }
+        }
+
+        public static int totalsalescount
+        {
+            get { return TotalSalesCount; }
+        }
+        public enum Currency
+        {
+            USD,
+            EURO,
+            TL
+        }
+
+       public enum Paymenttype
+        {
+            Card,
+            Cash
+        }
+
+        public static void AddSale(Currency currency, double amount)
+        {
+            TotalAmount = TotalAmount + CurrencyConverter(currency, amount);
+            TotalSalesCount++;
+        }
+
+        public static void RemoveSale(Currency currency, double amount)
+        {
+            TotalAmount = TotalAmount - CurrencyConverter(currency, amount);
+            TotalSalesCount++;
+        }
+
+
+        public static double CurrencyConverter(Currency currencytypes, double amount)
+        {
+            double azn = 0;
+            if (currencytypes == Currency.USD)
+            {
+                azn = amount * 1.7;
+                azn = Math.Round(azn, 2);
+            }
+            else
+                  if (currencytypes == Currency.EURO)
+            {
+                azn = amount * 1.92;
+                azn = Math.Round(azn, 2);
+            }
+            else
+                  if (currencytypes == Currency.TL)
+            {
+                azn = amount * 0.14;
+                azn = Math.Round(azn, 2);
+            }
+            return azn;
+        }
+    }
+
+
+}
---
+++ Kassa_Aparati_task_2/Kassa_Aparati_task_2/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kassa_Aparati_task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            CashRegister cashRegister = new CashRegister();
+            Console.WriteLine("Enter How many products do you ADD!!");
+           // CashRegister.AddSale(CashRegister.Currency.USD, 21);
+            int k = Convert.ToInt32(Console.ReadLine());
+            AddingElements(k);
+            Console.WriteLine("What do you want: Add or Remove Sell?");
+            Console.WriteLine("1-Add   2-Remove\nSelect one number:");
+            k = Convert.ToInt32(Console.ReadLine());
+
+            if (k != 1 && k != 2)
+            {
+                do
+                {
+                    Console.WriteLine("Please select RIGHT number: 1 or 2 !!!");
+                    k = Convert.ToInt32(Console.ReadLine());
+                } while (k != 1 && k != 2);
+
+            }
+            if (k == 1)
+            {
+                AddingElements(k);
+            }
+            if (k == 2)
+            {
+                RemovingElements();
+
+            }
+
+
+            Console.ReadLine();
+
+        }
+
+        public static void RemovingElements()
+        {
+            Console.WriteLine($"You can select USD, EURO, TL \nYou have to Choose Currency and Amount for {1} times:");
+            string a;
+            double money;
+
+            for (int i = 0; i < 1; i++)
+            {
+                a = Console.ReadLine().ToUpper();
+
+                money = Convert.ToDouble(Console.ReadLine());
+
+                if (a.ToUpper() == "USD")
+                {
+                    CashRegister.RemoveSale(CashRegister.Currency.USD, money);
+                }
+                else
+                     if (a.ToUpper() == "EURO")
+                {
+                    CashRegister.RemoveSale(CashRegister.Currency.EURO, money);
+                }
+                else
+                     if (a.ToUpper() == "TL")
+                {
+                    CashRegister.RemoveSale(CashRegister.Currency.TL, money);
+                }
+                else Console.WriteLine("You Would Choose Correct Currency Type!!!");
+
+            }
+
+            Console.WriteLine($" totalAmount: {CashRegister.totalamount} \nTotalSalesCount: {CashRegister.totalsalescount}");
+        }
+        public static void AddingElements(int k)
+        {
+            Console.WriteLine($"You can select USD, EURO, TL \nYou have to Choose Currency and Amount for {k} times:");
+            string a;
+            double money;
+
+            for (int i = 0; i < k; i++)
+            {
+                a = Console.ReadLine().ToUpper();
+
+                money = Convert.ToDouble(Console.ReadLine());
+
+                if (a.ToUpper() == "USD")
+                {
+                    CashRegister.AddSale(CashRegister.Currency.USD, money);
+                }
+                else
+                     if (a.ToUpper() == "EURO")
+                {
+                    CashRegister.AddSale(CashRegister.Currency.EURO, money);
+                }
+                else
+                     if (a.ToUpper() == "TL")
+                {
+                    CashRegister.AddSale(CashRegister.Currency.TL, money);
+                }
+                else Console.WriteLine("You Would Choose Correct Currency Type!!!");
+
+            }
+
+            Console.WriteLine($" totalAmount: {CashRegister.totalamount} \nTotalSalesCount: {CashRegister.totalsalescount}");
+        }
+
+    }
+}
