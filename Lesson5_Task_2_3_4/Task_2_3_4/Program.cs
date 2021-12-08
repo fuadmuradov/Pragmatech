3f616a006091fa3c957ca72826d9366281dd2290
fuadmuradov <63427158+fuadmuradov@users.noreply.github.com>
2021-12-08 23:27:45 +0400

    Add files via upload

---
+++ Lesson5_Task_2_3_4/Task_2_3_4/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2_3_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region //Task_2
+            //Queue<string> queue = new Queue<string>();
+
+            //queue.Enqueue("fuad1");
+            //queue.Enqueue("fuad2");
+            //queue.Enqueue("fuad3");
+            //queue.Enqueue("fuad4");
+            //queue.Enqueue("fuad5");
+
+            //string str = Console.ReadLine();
+            //if (queue.Count <= 10)
+            //{
+            //    queue.Enqueue(str);
+            //}
+            //else
+            //{
+            //    queue.Dequeue();
+            //    queue.Enqueue(str);
+            //}
+
+
+            //foreach (var item in queue)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //queue.Dequeue();
+            //Console.WriteLine();
+            //foreach (var item in queue)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //Console.WriteLine(queue.Count);
+
+            #endregion
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
