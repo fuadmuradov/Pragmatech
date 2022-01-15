38cd0392b1fb27979c26d5dd74a222b1d75df8fd
fuadmuradov <63427158+fuadmuradov@users.noreply.github.com>
2022-01-15 13:41:02 +0400

    Add files via upload
    Course Operation System. Student Add, Delete, Search, Update using Connected Model(SqlConnection, SqlCommand, ExecuteReader)

---
+++ Operations/Ins_Upda_Delete/Ins_Upda_Delete/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿
+namespace Ins_Upda_Delete
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtRoll = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCourse = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtRoll
+            // 
+            this.txtRoll.Location = new System.Drawing.Point(291, 57);
+            this.txtRoll.Name = "txtRoll";
+            this.txtRoll.Size = new System.Drawing.Size(219, 22);
+            this.txtRoll.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(189, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Roll Number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(189, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Student Name";
+            // 
+            // TxtName
+            // 
+            this.TxtName.Location = new System.Drawing.Point(291, 109);
+            this.TxtName.Name = "TxtName";
+            this.TxtName.Size = new System.Drawing.Size(219, 22);
+            this.TxtName.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(189, 166);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Age";
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(291, 161);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(219, 22);
+            this.txtAge.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(189, 223);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Course";
+            // 
+            // txtCourse
+            // 
+            this.txtCourse.Location = new System.Drawing.Point(291, 218);
+            this.txtCourse.Name = "txtCourse";
+            this.txtCourse.Size = new System.Drawing.Size(219, 22);
+            this.txtCourse.TabIndex = 6;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(81, 284);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 8;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(200, 284);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 9;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(334, 284);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Delete";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(457, 284);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Search";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(574, 284);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 12;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtCourse);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtRoll);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtRoll;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCourse;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnUpdate;
+    }
+}
+
---
+++ Operations/Ins_Upda_Delete/Ins_Upda_Delete/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ins_Upda_Delete
+{
+    public partial class Form1 : Form
+    {
+        string constring = ConfigurationManager.ConnectionStrings["rgstrform"].ConnectionString;
+        SqlConnection sqlConnection = null;
+        SqlCommand sqlCommand = null;
+        SqlDataReader sqlDataReader = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtRoll.Text = "";
+            txtCourse.Text = "";
+            TxtName.Text = "";
+            txtAge.Text = "";
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+
+            SqlConnection sqlConnection = new SqlConnection(constring);
+           
+
+            if (txtRoll.Text != "" && txtCourse.Text != "" && TxtName.Text != "" && txtAge.Text != "")
+            {
+                string name = TxtName.Text;
+                int roll = Convert.ToInt32(txtRoll.Text);
+                string Course = txtCourse.Text;
+                int Age = Convert.ToInt32(txtAge.Text);
+
+                sqlCommand = new SqlCommand("Insert into Student2 values(" + roll + ", '" + name + "', " + Age + ", '" + Course + "');", sqlConnection);
+                sqlConnection.Open();
+                sqlCommand.ExecuteNonQuery();
+                sqlConnection.Close();
+
+               
+            }
+            else
+            {
+                MessageBox.Show("Fill All Textbox!!!", "Warning", MessageBoxButtons.OK);
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (txtRoll.Text != "") {
+
+                int roll = Convert.ToInt32(txtRoll.Text);
+                sqlConnection = new SqlConnection(constring);
+                using (sqlCommand = new SqlCommand("Delete from Student2 where roll = " + roll + ";", sqlConnection))
+                {
+                    sqlConnection.Open();
+                    sqlCommand.ExecuteNonQuery();
+                    sqlConnection.Close();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Fill Roll Number", "Warning", MessageBoxButtons.OK);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (txtRoll.Text != "")
+            {
+
+                int roll = Convert.ToInt32(txtRoll.Text);
+                sqlConnection = new SqlConnection(constring);
+                using (sqlCommand = new SqlCommand("Select * from Student2 where roll = " + roll + ";", sqlConnection))
+                {
+                    sqlConnection.Open();
+                   sqlDataReader =  sqlCommand.ExecuteReader();
+                    if (sqlDataReader.Read())
+                    {
+                        TxtName.Text = sqlDataReader[1].ToString();
+                    }
+                    
+
+                    sqlConnection.Close();
+                    
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Fill Roll Number", "Warning", MessageBoxButtons.OK);
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (txtRoll.Text != "")
+            {
+
+                int roll = Convert.ToInt32(txtRoll.Text);
+                sqlConnection = new SqlConnection(constring);
+                using (sqlCommand = new SqlCommand("Update Student2 set name = '" + TxtName.Text +"' " + "where roll = " + roll + ";", sqlConnection))
+                {
+                    sqlConnection.Open();
+                    sqlCommand.ExecuteNonQuery();
+                    sqlConnection.Close();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Fill Roll Number", "Warning", MessageBoxButtons.OK);
+            }
+        }
+    }
+}
