16d824032a4695b9828f0ad04fb7e30b7d548f68
fuadmuradov <63427158+fuadmuradov@users.noreply.github.com>
2022-01-15 13:46:01 +0400

    Add files via upload
    EntittyFramework using DataBaseFirst. add to Combobox DataSource and DataGridView Datasource from Parfum database Parfum names

---
+++ EntityFramework/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmb1 = new System.Windows.Forms.ComboBox();
+            this.cmb2 = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmb1
+            // 
+            this.cmb1.FormattingEnabled = true;
+            this.cmb1.Location = new System.Drawing.Point(72, 59);
+            this.cmb1.Name = "cmb1";
+            this.cmb1.Size = new System.Drawing.Size(121, 24);
+            this.cmb1.TabIndex = 0;
+            this.cmb1.SelectedIndexChanged += new System.EventHandler(this.cmb1_SelectedIndexChanged);
+            // 
+            // cmb2
+            // 
+            this.cmb2.FormattingEnabled = true;
+            this.cmb2.Location = new System.Drawing.Point(315, 59);
+            this.cmb2.Name = "cmb2";
+            this.cmb2.Size = new System.Drawing.Size(121, 24);
+            this.cmb2.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 269);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(639, 150);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.cmb2);
+            this.Controls.Add(this.cmb1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmb1;
+        private System.Windows.Forms.ComboBox cmb2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ EntityFramework/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp1.Models;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private readonly ParfumsEntities db;
+        public Form1()
+        {
+            InitializeComponent();
+            db = new ParfumsEntities();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmb1.DataSource = db.Brands.Select(x => x.Name).ToList();
+            // dataGridView1.DataSource = db.Parfums.ToList();
+            dataGridView1.DataSource = db.Parfums.Select(x => x.Name).ToList();
+            //cmb1.DataSource = db.ParfumInfoes.Select(x => x.Name).ToList();
+                //  cmb1.DataSource = db.Parfums.Select(x => x.Name).ToList();
+           // List<string> parfums = db.Parfums.Select(x => x.Name).ToList();
+           // cmb1.DataSource = parfums;
+        }
+
+        private void cmb1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string brandname = cmb1.Text;
+
+            if (!string.IsNullOrEmpty(brandname))
+            {
+                Brand brandresult = db.Brands.FirstOrDefault(x => x.Name == brandname);
+                if (brandresult.Parfums.Count == 0)
+                {
+                    MessageBox.Show("brand adinda parfum yoxdur");
+                }
+                else
+                {
+                    cmb2.DataSource = brandresult.Parfums.Select(x => x.Name).ToList();
+                }
+            }
+        }
+    }
+}
